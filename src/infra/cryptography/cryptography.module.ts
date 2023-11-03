aecbc9923124deecdac4bff3914201e1c2231e96
Dener Araujo (Natsume) <natsume2b.me@gmail.com>
2023-11-03 17:10:09 -0300

    Controller & E2E Test: Authenticate

--- src/infra/cryptography/cryptography.module.ts
+++ src/infra/cryptography/cryptography.module.ts
@@ -1,15 +1,16 @@
 import { Module } from '@nestjs/common';
-
+import { Encrypter } from '@/domain/main/application/cryptography/encrypter';
+import { JwtEncrypter } from './jwt-encrypter';
 import { HashComparer } from '@/domain/main/application/cryptography/hash-comparer';
 import { HashGenerator } from '@/domain/main/application/cryptography/hash-generator';
-
 import { BcryptHasher } from './bcrypt-hasher';
 
 @Module({
   providers: [
+    { provide: Encrypter, useClass: JwtEncrypter },
     { provide: HashComparer, useClass: BcryptHasher },
     { provide: HashGenerator, useClass: BcryptHasher },
   ],
-  exports: [HashComparer, HashGenerator],
+  exports: [Encrypter, HashComparer, HashGenerator],
 })
 export class CryptographyModule {}
--- src/infra/http/http.module.ts
+++ src/infra/http/http.module.ts
@@ -3,9 +3,11 @@ import { DatabaseModule } from '../database/database.module';
 import { CreateAccountController } from './controllers/create-account.controller';
 import { RegisterOrgUseCase } from '@/domain/main/application/use-cases/register-org';
 import { CryptographyModule } from '../cryptography/cryptography.module';
+import { AuthenticateController } from './controllers/authenticate.controller';
+import { AuthenticateOrgUseCase } from '@/domain/main/application/use-cases/authenticate-org';
 @Module({
   imports: [DatabaseModule, CryptographyModule],
-  controllers: [CreateAccountController],
-  providers: [RegisterOrgUseCase],
+  controllers: [CreateAccountController, AuthenticateController],
+  providers: [RegisterOrgUseCase, AuthenticateOrgUseCase],
 })
 export class HttpModule {}
